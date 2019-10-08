bcf0874f7f0ba9375b7c5b261992ba8f86f361c5
Sam <super8989@gmail.com>
2019-10-07 22:14:47 -0400

    button list exercise

---
+++ src/ButtonList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './ButtonList.css';
+
+class ButtonList extends Component {
+  static defaultProps = {
+    colors: ["#e056fd", "#eb4d4b", "#badc58", "#f0932b"]
+  }; 
+
+  render() {
+    return (
+      <div className='ButtonList'>
+        {this.props.colors.map(c => {
+          const colorObj = { backgroundColor: c };
+          return <button style={colorObj}>Click Me</button>;
+        })}
+      
+      </div>
+    )
+  }
+}
+
+export default ButtonList;
